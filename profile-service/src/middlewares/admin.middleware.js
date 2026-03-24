d2ef529e06875266b97ddd4bfccdf12a5f9cbced
Athov07 <atharvadsare17@gmail.com>
2026-03-24 16:17:18 +0530

    profile-service - admin middleware config

--- profile-service/src/middlewares/admin.middleware.js
+++ profile-service/src/middlewares/admin.middleware.js
@@ -2,8 +2,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const isAdmin = asyncHandler(async (req, res, next) => {
-    console.log("Admin Check for user:", req.user?._id);
-    console.log("Role:", req.user?.role);
     if (!req.user) {
         throw new ApiError(401, "Unauthorized request: No user found");
     }
